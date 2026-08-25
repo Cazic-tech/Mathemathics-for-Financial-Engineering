336a00dc850714d7c8d7ee11fe61241bfecfb13e
Cazic-tech <christianazizcampos@gmail.com>
2026-08-25 10:29:35 -0400

    Update Bond_Metrics_Calculator.py

--- Chapter 2 - Numerical Integration, Interest Rates, and Bonds/Bond_Metrics_Calculator.py
+++ Chapter 2 - Numerical Integration, Interest Rates, and Bonds/Bond_Metrics_Calculator.py
@@ -1,13 +1,13 @@
 import math
 
-# Pricing bond and metrics based on giving yield
+# Pricing bond and metrics based on given yield
 
 #INPUTS#
 
 T = 14/12 #Bond maturity in years
 n = 5 #Number of Cash Flows
-t_cf = [2/12,5/12,8/12,11/12,14/12]
-v_cf = [2,2,2,2,102]
+t_cf = [2/12,5/12,8/12,11/12,14/12]  #timing of cash-flows, annual base
+v_cf = [2,2,2,2,102]  #value of cashflows 
 face_value = 100
 y = 0.07 #yield of the bond
 
